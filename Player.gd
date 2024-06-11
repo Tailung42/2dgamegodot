e6dd20bbcf928a80eb268d613201f4e46f456080
Bipin Thapa <tailung12345678910@gmail.com>
2024-06-11 07:13:20 +0545

    player is done

--- Player.gd
+++ Player.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+signal hit()
 @export var speed = 400
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
+	start(position)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -33,4 +36,21 @@ func _process(delta):
 
 
 	# Play the appropriate animation.
-	#devil of the paradise
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+		$AnimatedSprite2D.flip_v = false
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+	
+
+func _on_body_entered(body:Node2D):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true) 
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
