9ccf59fb843a268d17cce2a1b8bfd34110d7ec62
Bipin Thapa <tailung12345678910@gmail.com>
2024-06-14 17:19:24 +0545

    basic gameplay is ready

---
+++ main.gd
@@ -0,0 +1,49 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func gave_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_mob_timer_timeout():
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn_location = $MobPath/MobspawnLocation
+	mob_spawn_location.progress_ratio = randf()
+
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	mob.position = mob_spawn_location.position
+
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	add_child(mob)
+
+
+func _on_score_timer_timeout():
+	score += 1
+
+
+func _on_start_timer_timeout():
+	$MobTimer.start()	
+	$ScoreTimer.start()
