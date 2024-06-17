3fe0234cd224275265c0e839f69a920f3daf0c34
Bipin Thapa <tailung12345678910@gmail.com>
2024-06-17 11:34:21 +0545

    creaded head's up display

---
+++ HUD.gd
@@ -0,0 +1,40 @@
+extends CanvasLayer
+
+signal start_game
+
+func show_message(text):
+	$Message.text = text
+	$MessageTimer.start()
+	$Message.show()
+
+
+func show_game_over():
+	show_message("Game Over")
+	await $messageTimer.Timeout
+
+	$Message.text = "Dodge the Creeps!"
+	if is_inside_tree():
+		await get_tree().create_timer(1).timeout
+		$StartButton.show()
+
+
+func update_score(score):
+	$scoreLabel.text = str(score)
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+
+func _on_message_timer_timeout():
+	$Message.hide()
