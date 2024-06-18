6df5ca257710cabd910acf9ef047139eb6be5773
Bipin Thapa <tailung12345678910@gmail.com>
2024-06-18 10:12:05 +0545

    extended viewport to landscape

--- main.gd
+++ main.gd
@@ -26,6 +26,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
